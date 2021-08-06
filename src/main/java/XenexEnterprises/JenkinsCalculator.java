b308ce8b9c716419ea87c7b031233bc613decb97
Malinder Singh <malinder.singh@xenex.ca>
2021-08-06 00:32:26 -0400

    creating a new maven project

---
+++ src/main/java/XenexEnterprises/JenkinsCalculator.java
@@ -0,0 +1,12 @@
+package XenexEnterprises;
+
+public class JenkinsCalculator {
+
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+}
